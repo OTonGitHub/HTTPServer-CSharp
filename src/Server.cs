15d417c9af8f0ae0ce39b4ff5fd89c2f126afd78
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-06-05 03:46:48 +0000

    send 200 response on connection

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,16 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+Console.WriteLine("Program Entry");
 
-// Uncomment this block to pass the first stage
+const string HTTP_OK_STATUS_LINE = "HTTP/1.1 200 OK";
+
 TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+    using var socket = await server.AcceptSocketAsync();
+    await socket.SendAsync(Encoding.UTF8.GetBytes($"{HTTP_OK_STATUS_LINE}\r\n\r\n"), SocketFlags.None);
+}
